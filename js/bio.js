49707fcc004e87c02c9381dc55c8fe63949e39d8
CG <congodsted@protonmail.com>
2023-09-18 13:47:27 -0400

    Move officer bios into a popup.

---
+++ js/bio.js
@@ -0,0 +1,33 @@
+//opens the specified menu
+function openBio(bio) {
+    document.getElementById(bio).className = "visible-bio";
+    
+    document.getElementById(bio).style.opacity = 0;
+    let opacity = parseFloat(document.getElementById(bio).style.opacity)
+    setInterval(function(){
+        if (opacity <= 1) {
+            opacity += 1
+            document.getElementById(bio).style.opacity = opacity
+        }
+    }, 15)
+
+
+    }
+    
+//closes the specified menu
+function closeBio(bio) {
+    let opacity = parseFloat(document.getElementById(bio).style.opacity)
+    setInterval(function(){
+        if (opacity >= -5) {
+            opacity -= 1
+            document.getElementById(bio).style.opacity = opacity;
+            if (opacity <= -5) {
+                document.getElementById(bio).className = "hidden-bio";
+            }
+        }
+        }, 15)
+    
+    
+}
+   
+       
