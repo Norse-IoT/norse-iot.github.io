941690ac657f27486efbdc4140b40f6c873a8db5
Cassian Godsted <cassian@godsted.com>
2024-03-14 14:11:20 -0400

    Dark mode
    - Fixed width of looking to join section
    - Moved colour properties to CSS variables.
    - Fixed text overflow issue on microcontrollers text
    - Fixed incorrect credits

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+
+//This section of the code handles the Dark Mode popup.
+document.querySelector("body").innerHTML += `
+<div tabindex=0 onclick="theme('dark')" id="iot-theme" src="/images/dark.webp" class="" alt="Dark Mode">
+<i class="fa fa-2x fa-moon-o" aria-hidden="true"></i>
+</div>`
+
+//Checks if the user has already turned on Dark Mode.
+if (localStorage.darkMode == "true") {
+    theme("dark")
+}
+else {
+    theme("light")
+}
+
+
+//This function switches Dark Mode on and off.
+function theme(theme) {
+    baseurl = ``
+    //DARK MODE
+    if (theme == "dark") {
+        document.getElementById("theme").href = `${baseurl}/css/dark.css`
+        document.getElementById("iot-theme").outerHTML = `<div tabindex=0 onclick="theme('light')" id="iot-theme" src="/images/dark.webp" class="" alt="Light Mode">
+        <i class="fa fa-3x fa-sun-o" aria-hidden="true"></i>
+        </div>`
+        localStorage.darkMode = "true"
+    }
+    //LIGHT MODE
+    else {
+        localStorage.darkMode = "false"
+        document.getElementById("theme").href = `${baseurl}/css/light.css`
+        document.getElementById("iot-theme").outerHTML = `<div tabindex=0 onclick="theme('dark')" id="iot-theme" src="/images/dark.webp" class="" alt="Dark Mode">
+        <i class="fa fa-3x fa-moon-o" aria-hidden="true"></i>
+        </div>`
+
+
+    }
+}
+
 //opens the specified menu
 function openBio(bio) {
     //close a previously opened bio
